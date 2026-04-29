b4124d03e08f5baef599a91389b5db7a5b57a79f
Lorie-16 <loriecatamisan@gmail.com>
2026-04-29 15:51:00 +0000

    created pilot class

---
+++ Pilot.java
@@ -0,0 +1,55 @@
+/**
+ * Pilot crew member who can boost ship speed and perform navigation duties.
+ */
+public class Pilot extends CrewMember implements Boostable {
+
+    private int shipSpeed;
+
+    /**
+     * Creates a new pilot.
+     *
+     * @param name the pilot's name
+     * @param hp the pilot's health points
+     * @param role the pilot's role description
+     * @param shipSpeed the pilot's current ship speed
+     */
+    public Pilot(String name, int hp, String role, int shipSpeed) {
+        super(name, hp, role);
+        this.shipSpeed = shipSpeed;
+    }
+
+    /**
+     * Uses the pilot's special ability.
+     */
+    @Override
+    public void useAbility() {
+        System.out.println(getName() + " performs a precision maneuver at speed " + shipSpeed + ".");
+    }
+
+    /**
+     * Returns a description of the pilot's role.
+     *
+     * @return the role description
+     */
+    @Override
+    public String describeRole() {
+        return "Pilot: " + getRole();
+    }
+
+    /**
+     * Activates the pilot's boost ability.
+     */
+    @Override
+    public void activateBoost() {
+        shipSpeed += 10;
+        System.out.println(getName() + " activates boost. Ship speed is now " + shipSpeed + ".");
+    }
+
+    public int getShipSpeed() {
+        return shipSpeed;
+    }
+
+    public void setShipSpeed(int shipSpeed) {
+        this.shipSpeed = shipSpeed;
+    }
+}
