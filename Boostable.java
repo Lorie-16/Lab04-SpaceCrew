6a0e362e8c5440b959b63e2330b8cba7a9eb5de7
Lorie-16 <loriecatamisan@gmail.com>
2026-04-29 15:47:06 +0000

    generate absttract class

---
+++ Boostable.java
@@ -0,0 +1,10 @@
+/**
+ * Represents an object that can activate a temporary boost.
+ */
+public interface Boostable {
+
+    /**
+     * Activates the boost ability.
+     */
+    void activateBoost();
+}
